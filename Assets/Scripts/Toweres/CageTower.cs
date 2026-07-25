e6b1b2cff459c0294fdfdd1a457b41a30dcd0fd7
ndog <nathanbourke6@gmail.com>
2026-07-24 21:59:50 -0700

    bird no anims

--- Assets/Scripts/Toweres/CageTower.cs
+++ Assets/Scripts/Toweres/CageTower.cs
@@ -74,7 +74,9 @@ public class CageTower : MonoBehaviour
         }
 
         GameObject enemy = FindTaggedEnemy(other);
-        if (enemy != null && enemy.GetComponent<Enemy>().isCagable)
+        if (enemy != null
+            && enemy.TryGetComponent(out Enemy enemyComponent)
+            && enemyComponent.CanBeCaged)
         {
             Capture(enemy);
         }
@@ -166,6 +168,7 @@ public class CageTower : MonoBehaviour
             }
         }
 
+        enemy.GetComponent<Enemy>()?.OnReleasedFromCage();
         GetWaveSpawner()?.AddLivingEnemy(enemy);
         SetEnemySorting(enemy, "Enemy");
         disabledEnemyColliders.Clear();
